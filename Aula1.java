f1f3917c9052e326c9e7bf67f56916060ec0b057
bavichugo <victorhugobatistela@gmail.com>
2021-11-13 19:08:06 -0500

    Aula 1 commit

---
+++ Aula1.java
@@ -0,0 +1,8 @@
+public class Aula1 {
+    public static void main(String[] args) {
+        System.out.println("Oi");
+
+        // Essa linha fala tudo bem
+        System.out.println("Tudo bem?");
+    }
+}
---
+++ Aula2.java
@@ -0,0 +1,42 @@
+public class Aula2 {
+    public static void main(String[] args) {
+        // Nome das variaveis comeca com letra e pode seguir com letras
+        // numeros e underlines (_)
+        int num;
+        double num2;
+        char a_letter;
+        // long 123abc;
+        char letra;
+
+        // As variaveis sao case-sensitive
+        int Num;
+        int nUm;
+
+        // Estilos de nomes das variaveis
+        // camelCase
+        String primeiroNome;
+        String corDoCabelo;
+
+        // Underline como espaco
+        String primeiro_nome;
+        String cor_do_cabelo;
+
+        // Declaracao
+        int idade;
+
+        // inicializacao
+        idade = 5;
+
+        // Declaracao e inicializacao
+        int ano = 2020;
+
+        // Criar constantes
+        // Use a keyword final
+        final double PI = 3.14159;
+        final int TOTAL_DE_JOGOS_NA_TEMPORADA = 13;
+
+        // Tipagem Estáticas
+        // Não permite alterar o tipo da variável depois de declarada.
+        String nome = "Victor";
+    }
+}
